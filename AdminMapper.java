69f357dd4138708bb4f1e50a2a0ce0db1fc638d6
Nanfeng321 <132559678+Nanfeng321@users.noreply.github.com>
2023-06-28 15:44:27 +0800

    Mapper

---
+++ AdminMapper.java
@@ -0,0 +1,12 @@
+package com.milotnt.mapper;
+
+import com.milotnt.pojo.Admin;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface AdminMapper {
+
+    Admin selectByAccountAndPassword(Admin admin);
+
+}
